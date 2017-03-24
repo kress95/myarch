3bb7f943eb10a3661e5c5b9528605aaf3eb9c80a
kress95 <cferraz95@gmail.com>
2017-03-24 19:42:39 -0300

    Create list builder.

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+rm -f *.cache
+
+pacman -Q \
+| sed 's/ .*//' \
+| sort \
+> 0.cache
+
+cat ./*/*.pkgs \
+> 1.cache
+
+xargs -0 -n 1 pactree -lu \
+< <(tr \\n \\0 <1.cache) \
+> 2.cache
+
+cat 1.cache 2.cache \
+> 3.cache
+
+cat 3.cache \
+| sort \
+| uniq \
+> 4.cache
+
+comm -13 4.cache 0.cache \
+> 5.cache
+
+printf "\n"
+
+printf "Remove with:\n\$ sudo pacman -Rcn "
+
+cat 5.cache \
+| tr '\r\n' ' '
+
+printf "\n"
+
+rm -f *.cache
